56179da21a233326cb1d487afa27239348430e32
shohagnawaz <snawazdigital@gmail.com>
2021-09-01 18:22:36 +0600

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements';
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <>
+            <IconContext.Provider value = {{ color: '#fff' }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to='/'>
+                            <NavIcon>
+                                ULTRA
+                            </NavIcon>
+                            <MobileIcon onClick = {handleClick}>
+                                {click ? <FaTimes /> : <FaBars />}
+                            </MobileIcon>
+                            <NavMenu onClick = {handleClick}>
+                                <NavItem>
+                                    <NavLinks to='/'>Home</NavLinks>
+                                </NavItem>
+                            </NavMenu>
+                        </NavLogo>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/globalStyles.js
@@ -0,0 +1,27 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Source San Pro', sans-serif;    
+}
+`;
+
+export const Container = styled.div`
+z-index: 1;
+width: 100%;
+max-width: 1300px;
+margin-right: auto;
+margin-left: auto;
+padding-right: 50px;
+padding-left: 50px;
+
+@media screen and (max-width: 991px) {
+    padding-right: 30%;
+    padding-left: 30%;
+}
+`;
+
+export default GlobalStyle;
