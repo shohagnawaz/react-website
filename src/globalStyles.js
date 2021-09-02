60d00d86de1057ed4d502948a762579860f7a634
shohagnawaz <snawazdigital@gmail.com>
2021-09-02 18:15:25 +0600

    Navbar

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -10,18 +10,23 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export const Container = styled.div`
-z-index: 1;
-width: 100%;
-max-width: 1300px;
-margin-right: auto;
-margin-left: auto;
-padding-right: 50px;
-padding-left: 50px;
+    z-index: 1;
+    width: 100%;
+    max-width: 1300px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 50px;
+    padding-left: 50px;
 
-@media screen and (max-width: 991px) {
-    padding-right: 30%;
-    padding-left: 30%;
-}
+    @media screen and (max-width: 991px) {
+        padding-right: 30%;
+        padding-left: 30%;
+    }
+`;
+
+export const Button = styled.button`
+    border-radius: 4px;
+    backgournd: ${({primary}) => (primary ? '#4B59F7' : '#0467FB') }
 `;
 
 export default GlobalStyle;
